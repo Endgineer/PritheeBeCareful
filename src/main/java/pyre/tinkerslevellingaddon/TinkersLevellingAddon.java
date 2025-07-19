5c83d4476c04520d6d8642fc8396b38ffc02e093
Endgineer <bm.alkhudairy@hotmail.com>
2025-07-19 04:37:39 -0400

    Implemented specs datagen.

--- src/main/java/pyre/tinkerslevellingaddon/TinkersLevellingAddon.java
+++ src/main/java/pyre/tinkerslevellingaddon/TinkersLevellingAddon.java
@@ -24,11 +24,14 @@ import pyre.tinkerslevellingaddon.core.PbcBlocks;
 import pyre.tinkerslevellingaddon.core.PbcCreativeModeTabs;
 import pyre.tinkerslevellingaddon.core.PbcItems;
 import pyre.tinkerslevellingaddon.core.PbcMenus;
+import pyre.tinkerslevellingaddon.core.PbcSpecs;
 import pyre.tinkerslevellingaddon.data.PbcBlockTagsProvider;
 import pyre.tinkerslevellingaddon.data.PbcLootTableProvider;
 import pyre.tinkerslevellingaddon.data.PbcRecipeProvider;
 import pyre.tinkerslevellingaddon.loader.forging.ForgingMaterialSpecManager;
+import pyre.tinkerslevellingaddon.loader.forging.ForgingMaterialSpecProvider;
 import pyre.tinkerslevellingaddon.loader.reinforcing.ReinforcingGearSpecManager;
+import pyre.tinkerslevellingaddon.loader.reinforcing.ReinforcingGearSpecProvider;
 import pyre.tinkerslevellingaddon.network.Messages;
 import pyre.tinkerslevellingaddon.setup.Registration;
 
@@ -72,5 +75,11 @@ public class TinkersLevellingAddon {
         generator.addProvider(event.includeServer(), new PbcRecipeProvider(packOutput));
         generator.addProvider(event.includeServer(), new PbcBlockTagsProvider(packOutput, lookupProvider, existingFileHelper));
         generator.addProvider(event.includeServer(), new PbcLootTableProvider(packOutput));
+        
+        PbcSpecs.generateForgingMaterialSpecs();
+        generator.addProvider(event.includeServer(), new ForgingMaterialSpecProvider(packOutput));
+        
+        PbcSpecs.generateReinforcingGearSpecs();
+        generator.addProvider(event.includeServer(), new ReinforcingGearSpecProvider(packOutput));
     }
 }
