592f0e3341b46b33921186ea8acb0b664d22c9ff
Endgineer <bm.alkhudairy@hotmail.com>
2025-07-19 02:54:45 -0400

    Added block tags provider.

--- src/main/java/pyre/tinkerslevellingaddon/TinkersLevellingAddon.java
+++ src/main/java/pyre/tinkerslevellingaddon/TinkersLevellingAddon.java
@@ -1,15 +1,20 @@
 package pyre.tinkerslevellingaddon;
 
 import com.mojang.logging.LogUtils;
+
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import pyre.tinkerslevellingaddon.command.ModCommands;
@@ -19,6 +24,7 @@ import pyre.tinkerslevellingaddon.core.PbcBlocks;
 import pyre.tinkerslevellingaddon.core.PbcCreativeModeTabs;
 import pyre.tinkerslevellingaddon.core.PbcItems;
 import pyre.tinkerslevellingaddon.core.PbcMenus;
+import pyre.tinkerslevellingaddon.data.PbcBlockTagsProvider;
 import pyre.tinkerslevellingaddon.data.PbcRecipeProvider;
 import pyre.tinkerslevellingaddon.loader.forging.ForgingMaterialSpecManager;
 import pyre.tinkerslevellingaddon.loader.reinforcing.ReinforcingGearSpecManager;
@@ -59,6 +65,10 @@ public class TinkersLevellingAddon {
     static void gatherData(final GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        
         generator.addProvider(event.includeServer(), new PbcRecipeProvider(packOutput));
+        generator.addProvider(event.includeServer(), new PbcBlockTagsProvider(packOutput, lookupProvider, existingFileHelper));
     }
 }
---
+++ src/main/java/pyre/tinkerslevellingaddon/data/PbcBlockTagsProvider.java
@@ -0,0 +1,32 @@
+package pyre.tinkerslevellingaddon.data;
+
+import java.util.concurrent.CompletableFuture;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import pyre.tinkerslevellingaddon.TinkersLevellingAddon;
+import pyre.tinkerslevellingaddon.core.PbcBlocks;
+
+public class PbcBlockTagsProvider extends BlockTagsProvider {
+    public PbcBlockTagsProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookupProvider, ExistingFileHelper existingFileHelper) {
+        super(packOutput, lookupProvider, TinkersLevellingAddon.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        tag(BlockTags.MINEABLE_WITH_PICKAXE).add(PbcBlocks.REINFORCEMENT_ANVIL.get());
+        tag(BlockTags.NEEDS_IRON_TOOL).add(PbcBlocks.REINFORCEMENT_ANVIL.get());
+        
+        tag(BlockTags.MINEABLE_WITH_PICKAXE).add(PbcBlocks.FORGE_CHAMBER.get());
+        tag(BlockTags.NEEDS_IRON_TOOL).add(PbcBlocks.FORGE_CHAMBER.get());
+        
+        tag(BlockTags.MINEABLE_WITH_PICKAXE).add(PbcBlocks.FORGE_HEARTH.get());
+        tag(BlockTags.NEEDS_IRON_TOOL).add(PbcBlocks.FORGE_HEARTH.get());
+        
+        tag(BlockTags.MINEABLE_WITH_PICKAXE).add(PbcBlocks.FORGE_THROAT.get());
+        tag(BlockTags.NEEDS_IRON_TOOL).add(PbcBlocks.FORGE_THROAT.get());
+    }
+}
